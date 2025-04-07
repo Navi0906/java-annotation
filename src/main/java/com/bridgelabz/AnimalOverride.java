9927b9d55c3c83264e62615f37b84d39e7cf2205
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-04-07 09:30:39 +0530

    Naam Simran Singh Gill Add: Java annotation

---
+++ src/main/java/com/bridgelabz/AnimalOverride.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+
+class Animal {
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+
+public class AnimalOverride {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.makeSound();
+    }
+}
+
